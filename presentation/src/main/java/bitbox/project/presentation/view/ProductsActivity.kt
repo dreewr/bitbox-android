9e19caca6c2c0fc4eef505a47279f3e86dee8e09
Deco Rodrigues <falecomandresantos@gmail.com>
2019-04-21 13:48:58 -0300

    Tela de produtos modificada e tela de verificação criada

---
+++ presentation/src/main/java/bitbox/project/presentation/view/ProductsActivity.kt
@@ -0,0 +1,13 @@
+package bitbox.project.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bitbox.project.presentation.R
+
+class ProductsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
---
+++ presentation/src/main/java/bitbox/project/presentation/view/VerificationActivity.kt
@@ -0,0 +1,13 @@
+package bitbox.project.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bitbox.project.presentation.R
+
+class VerificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification)
+    }
+}
